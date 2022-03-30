61ea7f040d33811e17f4b878252249ee4affe1c3
Hampus Vågeteg <hampusvageteg@gmail.com>
2022-03-30 10:39:55 +0200

    Lagt in alla affärsobjekt

---
+++ src/main/java/com/grupp5/grupp5sakila/entity/Film.java
@@ -0,0 +1,170 @@
+package com.grupp5.grupp5sakila.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "film")
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "film_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "release_year")
+    private Integer releaseYear;
+
+    @Column(name = "rental_duration", nullable = false)
+    private Integer rentalDuration;
+
+    @Column(name = "rental_rate", nullable = false, precision = 4, scale = 2)
+    private BigDecimal rentalRate;
+
+    @Column(name = "length")
+    private Integer length;
+
+    @Column(name = "replacement_cost", nullable = false, precision = 5, scale = 2)
+    private BigDecimal replacementCost;
+
+    @Lob
+    @Column(name = "rating")
+    private String rating;
+
+    @Lob
+    @Column(name = "special_features")
+    private String specialFeatures;
+
+    @Column(name = "last_update", nullable = false)
+    private Instant lastUpdate;
+
+    @OneToMany(mappedBy = "film")
+    private Set<Inventory> inventories = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "film")
+    private Set<FilmActor> filmActors = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "film")
+    private Set<FilmCategory> filmCategories = new LinkedHashSet<>();
+
+    public Set<FilmCategory> getFilmCategories() {
+        return filmCategories;
+    }
+
+    public void setFilmCategories(Set<FilmCategory> filmCategories) {
+        this.filmCategories = filmCategories;
+    }
+
+    public Set<FilmActor> getFilmActors() {
+        return filmActors;
+    }
+
+    public void setFilmActors(Set<FilmActor> filmActors) {
+        this.filmActors = filmActors;
+    }
+
+    public Set<Inventory> getInventories() {
+        return inventories;
+    }
+
+    public void setInventories(Set<Inventory> inventories) {
+        this.inventories = inventories;
+    }
+
+    public Instant getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Instant lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getSpecialFeatures() {
+        return specialFeatures;
+    }
+
+    public void setSpecialFeatures(String specialFeatures) {
+        this.specialFeatures = specialFeatures;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public BigDecimal getReplacementCost() {
+        return replacementCost;
+    }
+
+    public void setReplacementCost(BigDecimal replacementCost) {
+        this.replacementCost = replacementCost;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public BigDecimal getRentalRate() {
+        return rentalRate;
+    }
+
+    public void setRentalRate(BigDecimal rentalRate) {
+        this.rentalRate = rentalRate;
+    }
+
+    public Integer getRentalDuration() {
+        return rentalDuration;
+    }
+
+    public void setRentalDuration(Integer rentalDuration) {
+        this.rentalDuration = rentalDuration;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/grupp5/grupp5sakila/entity/Rental.java
@@ -0,0 +1,22 @@
+package com.grupp5.grupp5sakila.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rental")
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rental_id", nullable = false)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    //TODO Reverse Engineering! Migrate other columns to the entity
+}
